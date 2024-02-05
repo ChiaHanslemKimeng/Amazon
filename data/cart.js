6301246879dd32306eccddb5755383b7282e0634
ChiaHanslemKimeng <116301621+ChiaHanslemKimeng@users.noreply.github.com>
2024-02-05 12:59:39 +0100

    Add files via upload

---
+++ data/cart.js
@@ -0,0 +1,54 @@
+
+export const cart = 
+[{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+},
+{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
+
+// local storage ============================
+function saveTostorage(){
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+// add to cart function =================
+export function addToCart(productId){
+  let matchingItem;
+
+  cart.forEach((CartItem) => {
+    if (productId === CartItem.productId) {
+      matchingItem = CartItem;
+    }
+  })
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  }
+
+  else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    })
+  }
+
+  saveTostorage();
+}
+
+
+// remove from cart function ========
+export function removeFromCart(productId){
+  const newCart = [];
+  cart.forEach((CartItem) => {
+   if(CartItem.productId !== productId){
+    newCart.push(CartItem);
+   }
+  });
+
+  cart = newCart;
+
+  saveTostorage();
+}
